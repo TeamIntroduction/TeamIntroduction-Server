51dd97dda428e6c04680410a8e061a6380a9ae78
Junwoo Kwon <top7578@naver.com>
2023-01-11 09:20:29 +0900

    feat: CORS 설정 서버 처리에서 프록시 서버로 변경

--- src/main/java/com/example/project/config/CorsConfig.java
+++ src/main/java/com/example/project/config/CorsConfig.java
@@ -1,10 +1,6 @@
 package com.example.project.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,16 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods(
-                        HttpMethod.OPTIONS.name(),
-                        HttpMethod.GET.name(),
-                        HttpMethod.POST.name(),
-                        HttpMethod.PATCH.name(),
-                        HttpMethod.PUT.name(),
-                        HttpMethod.DELETE.name())
                 .allowCredentials(true)
-                .allowedHeaders("content-type", "Authorization")
-                .exposedHeaders(HttpHeaders.SET_COOKIE)
                 .maxAge(3000);
     }
 }
